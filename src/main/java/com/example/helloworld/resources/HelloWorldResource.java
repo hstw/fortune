9f9d0cc0027acdbadc88b8783a53a615a6c724f1
Xiaoyang Gu <xiaoyang@borderxlab.com>
2018-06-30 07:20:56 +0800

    more comments

--- src/main/java/com/example/helloworld/resources/HelloWorldResource.java
+++ src/main/java/com/example/helloworld/resources/HelloWorldResource.java
@@ -9,7 +9,7 @@ import javax.ws.rs.core.MediaType;
 import java.util.Optional;
 import java.util.concurrent.atomic.AtomicLong;
 
-@Path("/hello-world") // this is where the http path is set
+@Path("/hello-world") // This is where the HTTP URL path is set
 @Produces(MediaType.APPLICATION_JSON)
 public class HelloWorldResource {
     private static final Logger LOGGER = LoggerFactory.getLogger(HelloWorldResource.class);
@@ -26,7 +26,7 @@ public class HelloWorldResource {
      * curl  localhost:8080/hello-world?name=aaa
      * curl  localhost:8080/hello-world
      *
-     * @param name
+     * @param name Note that the URL parameter `name` you see above gets parsed to the `name` method parameter below.
      * @return
      */
     @GET
@@ -39,7 +39,7 @@ public class HelloWorldResource {
      * invoke by:
      * curl -XPOST localhost:8080/hello-world -H "Content-Type: application/json"   -H "Accept: application/json" -d '{"id":333}'
      *
-     * @param saying
+     * @param saying Note for HTTP POST, the payload data above are parsed into the `saying` method parameter below.
      */
     @POST
     public void receiveHello(@Valid Saying saying) {
@@ -49,16 +49,16 @@ public class HelloWorldResource {
     /**
      * HTTP DELETE
      * curl -XDELETE  'localhost:8080/hello-world?id=1234'
-     *
+     * Note that the URL parameter `id` you see above gets parsed to the `id` method parameter below.
      * @param id
      */
     @DELETE
     public void deleteIt(@QueryParam("id") Optional<String> id) {
         if (id.isPresent()) {
             LOGGER.info("delete object with id:=" + id.get());
+            System.out.println("delete object with id:=" + id.get());
         } else {
             LOGGER.info("delete. id not supplied");
-
         }
     }
 }
